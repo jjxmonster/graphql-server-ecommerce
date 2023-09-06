e826692ff639c2d3a30058f8fc3015eb5b1da12f
jjxmonster <kuba.tarabasz@wp.pl>
2023-09-06 13:51:45 +0200

    Configured graphql, created schema, apollo server init

---
+++ src/graphql/schema/resolvers/Query/product.ts
@@ -0,0 +1,16 @@
+import type { QueryResolvers } from "./../../../types.generated";
+import { faker } from "@faker-js/faker";
+export const product: NonNullable<QueryResolvers["product"]> = async (
+  _parent,
+  arg,
+  _ctx
+) => {
+  const name = faker.commerce.productName();
+  return {
+    id: arg.id,
+    name,
+    slug: faker.helpers.slugify(name),
+    price: faker.number.int({ min: 100, max: 10000 }),
+    description: faker.commerce.productDescription(),
+  };
+};
