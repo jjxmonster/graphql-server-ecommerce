41f843a8bb70455ef4dc56ec842c8b87f753d0f8
jjxmonster <kuba.tarabasz@wp.pl>
2023-10-04 17:34:17 +0200

    Delete orderItem mutation

--- src/graphql/schema/resolvers/Mutation/deleteOrderItem.ts
+++ src/graphql/schema/resolvers/Mutation/deleteOrderItem.ts
@@ -1,6 +1,17 @@
+import { prisma } from "../../../../db";
 import type { MutationResolvers } from "./../../../types.generated";
 export const deleteOrderItem: NonNullable<
   MutationResolvers["deleteOrderItem"]
 > = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.deleteOrderItem resolver logic here */
+  const order = await prisma.orderItem.delete({
+    where: {
+      id: _arg.id,
+    },
+  });
+
+  if (!order) {
+    return null;
+  }
+
+  return order.id;
 };
