c9221672b910d77cff4bc38c45b0d17da1ad3a8f
jjxmonster <kuba.tarabasz@wp.pl>
2023-10-09 21:10:54 +0200

    Delete order if all orderItems are deleted

--- src/graphql/schema/resolvers/Mutation/deleteOrderItem.ts
+++ src/graphql/schema/resolvers/Mutation/deleteOrderItem.ts
@@ -3,15 +3,32 @@ import type { MutationResolvers } from "./../../../types.generated";
 export const deleteOrderItem: NonNullable<
   MutationResolvers["deleteOrderItem"]
 > = async (_parent, _arg, _ctx) => {
-  const order = await prisma.orderItem.delete({
+  const orderItem = await prisma.orderItem.delete({
     where: {
       id: _arg.id,
     },
   });
 
-  if (!order) {
+  const order = await prisma.order.findUnique({
+    where: {
+      id: orderItem.orderId,
+    },
+    include: {
+      orderItems: true,
+    },
+  });
+
+  if (order?.orderItems.length === 0) {
+    await prisma.order.delete({
+      where: {
+        id: order.id,
+      },
+    });
+  }
+
+  if (!orderItem) {
     return null;
   }
 
-  return order.id;
+  return orderItem.id;
 };
